58f7c9331b72db08ae2e282362735ef98af8087d
lukeypap <luke.pap@live.co.uk>
2022-09-08 14:56:44 +0100

    Changed logo size on different screens

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className="flex items-center p-4 text-white fixed">
             <div>
                 <span>
-                    <p>
+                    <p className="text-lg lg:text-2xl">
                         <strong>L</strong>
                         <strong className="-translate-x-1 absolute">P</strong>
                     </p>
